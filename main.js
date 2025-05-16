9d13f0d2d2e695a7407525ba5b60349f63ff44b6
PIRON--MARIE Nathan <149869043+Nath75999@users.noreply.github.com>
2025-05-16 11:42:51 +0200

    Added completeTab function
    Maybe simplification is needed, it's not quite understandable right now

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+function completeTab(){
+    const tbody = document.querySelector('#tab tbody');
 
+    // Fusionner les deux tableaux
+    const allData = [...faune, ...flore];
+
+    // Insérer les données dans le tableau HTML
+    allData.forEach(obs => {
+    const row = document.createElement('tr');
+    row.innerHTML = `<td>${obs.NOM_SC}</td>
+    <td>${obs.NOM_FR}</td>
+    <td>${obs.COMMUNE}</td>
+    <td>${obs.CODE_INSEE}</td>
+    <td>${obs.DATE_OBS}</td>`;
+    tbody.appendChild(row);
+    });
+}
+
+function main(){
+    completeTab();
+}
+
+main();
